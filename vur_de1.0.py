8c0e75961f30d3d19df28c75243edf65a2388ddb
vivesm <34359730+vivesm@users.noreply.github.com>
2023-03-31 13:36:16 -0400

    Update vur_de1.0.py

--- vur_de1.0.py
+++ vur_de1.0.py
@@ -1,33 +1,22 @@
-################################################################
-# YouTube Downloader 1.0
-#  - Added type annotations for function arguments and return types to improve code clarity and maintainability.
-#  - Created separate functions for downloading video and audio files to improve code organization and readability.
-#  - Added file extension checks to ensure that the downloaded audio file is saved as an MP3 file.
-#  - Removed unnecessary imports and SSL context configuration to simplify the code and reduce the attack surface.
-#  - Changed the order of the except statements to more accurately reflect the error conditions being handled.
-#  - Refactored the download_selected() function to reduce code duplication and improve readability.
-#  - Changed the error message displayed when an invalid URL or unavailable video is selected to provide more specific information to the user.
-#  - Removed the progress bar from the UI and associated code to simplify the application and reduce clutter.
-
 import os
 import pytube
 from pytube.exceptions import VideoUnavailable, RegexMatchError
-import urllib.request
-import ssl
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import filedialog, messagebox
+import ssl
+import certifi
 
 ssl._create_default_https_context = ssl._create_unverified_context
-
+ssl.create_default_context = ssl._create_unverified_context
+ssl.get_default_verify_paths = lambda: ssl.Purpose.CLIENT_AUTH(certfile=certifi.where(), capath=None)
 
 def validate_url(url: str) -> bool:
     try:
         pytube.YouTube(url)
-    except (pytube.exceptions.VideoUnavailable, pytube.exceptions.RegexMatchError):
+    except (VideoUnavailable, RegexMatchError):
         return False
     return True
-
 
 def validate_input() -> bool:
     if not validate_url(video_url_entry.get()):
@@ -38,7 +27,6 @@ def validate_input() -> bool:
         return False
     return True
 
-
 def download_video(yt: pytube.YouTube) -> str:
     video_stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
     filename = f"{yt.title} (Video).mp4"
@@ -46,7 +34,6 @@ def download_video(yt: pytube.YouTube) -> str:
     if filepath:
         video_stream.download(output_path=os.path.dirname(filepath), filename=os.path.basename(filepath))
     return filepath
-
 
 def download_audio(yt: pytube.YouTube) -> str:
     audio_stream = yt.streams.filter(only_audio=True).first()
@@ -59,8 +46,8 @@ def download_audio(yt: pytube.YouTube) -> str:
     os.remove(audio_filename)
     return new_filename
 
-
 def download_selected() -> None:
+    global video_url_entry, video_var, audio_var
     if not validate_input():
         return
     url = video_url_entry.get()
@@ -82,61 +69,38 @@ def download_selected() -> None:
     except Exception as e:
         messagebox.showerror("Error", f"An error occurred while downloading the content:\n{str(e)}")
 
-
 def clear_url() -> None:
+    global video_url_entry, progress_bar
     video_url_entry.delete(0, tk.END)
     progress_bar.stop()
     progress_bar.grid_forget()
 
+def main():
+    global video_url_entry, video_var, audio_var, progress_bar
 
-root = tk.Tk()
-root.title("YouTube Downloader")
-root.resizable(False, False)
+    root = tk.Tk()
+    root.title("YouTube Downloader")
+    root.resizable(False, False)
 
-video_url_label = tk.Label(root, text="YouTube URL:")
-video_url_label.grid(row=0, column=0, padx=5, pady=5)
-video_url_entry = tk.Entry(root)
-video_url_entry.grid(row=0, column=1, padx=5, pady=5)
-clear_button = tk.Button(root, text="Clear", command=clear_url)
-clear_button.grid(row=0, column=2, padx=5, pady=5)
-video_var = tk.BooleanVar(value=True)
-video_check_button = tk.Checkbutton(root, text="Video", variable=video_var)
-video_check_button.grid(row=1, column=0, padx=5, pady=5, sticky='E')
-audio_var = tk.BooleanVar()
-audio_check_button = tk.Checkbutton(root, text="Audio", variable=audio_var)
-audio_check_button.grid(row=1, column=1, padx=5, pady=5, sticky='W')
+    video_url_label = tk.Label(root, text="YouTube URL:")
+    video_url_label.grid(row=0, column=0, padx=5, pady=5)
+    video_url_entry = tk.Entry(root)
+    video_url_entry.grid(row=0, column=1, padx=5, pady=5)
+    clear_button = tk.Button(root, text="Clear", command=clear_url)
+    clear_button.grid(row=0, column=2, padx=5, pady=5)
+    video_var = tk.BooleanVar(value=True)
+    video_check_button = tk.Checkbutton(root, text="Video", variable=video_var)
+    video_check_button.grid(row=1, column=0, padx=5, pady=5, sticky='E')
+    audio_var = tk.BooleanVar()
+    audio_check_button = tk.Checkbutton(root, text="Audio", variable=audio_var)
+    audio_check_button.grid(row=1, column=1, padx=5, pady=5, sticky='W')
 
-download_button = tk.Button(root, text="Download", command=download_selected)
-download_button.grid(row=1, column=2, padx=5, pady=5)
+    download_button = tk.Button(root, text="Download", command=download_selected)
+    download_button.grid(row=1, column=2, padx=5, pady=5)
 
-progress_bar = ttk.Progressbar(root, mode='determinate', orient=tk.HORIZONTAL, length=200)
+    progress_bar = ttk.Progressbar(root, mode='determinate', orient=tk.HORIZONTAL, length=200)
 
-root.mainloop()
+    root.mainloop()
 
-# Version 0.6.0b:
-# Improved error handling to provide more specific error messages to the user.
-# Improved input validation to ensure that the user has selected a file type to download.
-# Added download progress bar.
-
-# Version 0.5.0b:
-# Added "Video" or "Audio" label to downloaded file names to make it clear what type of file was downloaded.
-# Prevented the window from being resized to improve the appearance and usability of the application.
-
-# Version 0.4.0b:
-# Added input validation to ensure that users provide valid URLs.
-# Added error handling for invalid URLs and download errors.
-# Added the option to select the download location for the file.
-
-# Version 0.1.0b:
-# Added a button to clear the URL field.
-# Cleaned up the GUI to improve usability and appearance.
-# Fixed a bug where only the audio file was downloaded when both boxes were checked.
-
-# Version 0.0.3b:
-# Added the option to download video and/or audio.
-
-# Version 0.0.2b:
-# Added a GUI to the application.
-
-# Version 0.0.1a:
-# Initial release with the ability to download audio from YouTube in the command line interface.
+if __name__ == '__main__':
+    main()
